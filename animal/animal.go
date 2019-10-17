9f462819dfbee0041939e9b17480cfe6b3c02dc1
Matthew Johnson <mattrsjohnson@gmail.com>
2019-10-17 16:33:14 -0700

    Add comments to animal/animal.go

--- animal/animal.go
+++ animal/animal.go
@@ -1,16 +1,22 @@
 package animal
 
-// An Animal is a living organism that feeds on organic matter, typically having
+// Animal is a living organism that feeds on organic matter, typically having
 // specialized sense organs and nervous system and able to respond rapidly to
 // stimuli.
 type Animal interface {
 	// Commands
+	// Speak returns the sound the animal makes
 	Speak() string
+	// Eat returns the sound the animal makes while eating and may modify weight
 	Eat() string
+	// Bite returns the sound the animal makes while biting
 	Bite() string
 
 	// Reporting functions
+	// Name returns the name of the animal
 	Name() string
+	// Weight returns the weight (in lbs) of the animal
 	Weight() int
+	// Age returns the age (in years) of the animal
 	Age() int
 }
