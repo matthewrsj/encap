4a47a5cf735e121ddc3425d105bd0f6b30d81586
Matthew Johnson <mattrsjohnson@gmail.com>
2019-10-17 16:29:28 -0700

    Add comments to main file

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/matthewrsj/encap/animal"
 )
 
+// report is the template to output information to the command line
 const report = `
 weight: {{.Weight}} lbs
 age:    {{.Age}} yrs
@@ -24,6 +25,7 @@ bite {{.Name}}!  "{{.Bite}}"
 
 `
 
+// Report takes an Animal interface and prints it out via the template report
 func Report(a animal.Animal) {
 	tp := strings.TrimPrefix(reflect.TypeOf(a).String(), "*animal.")
 	fmt.Printf("%s is a %s that can speak, eat, and bark!\n", a.Name(), tp)
@@ -35,6 +37,7 @@ func Report(a animal.Animal) {
 }
 
 func main() {
+	// each animal is a different type, but all animals implement the animal.Animal interface
 	animals := []animal.Animal{
 		animal.NewDog("Rover", 50, 5),
 		animal.NewShowDog("Princess", 5, 2),
@@ -42,6 +45,7 @@ func main() {
 		animal.NewGopher("Henry", 3, 1),
 	}
 
+	// since each animal implements the animal.Animal interface they can all be passed to Report()
 	for _, a := range animals {
 		Report(a)
 	}
